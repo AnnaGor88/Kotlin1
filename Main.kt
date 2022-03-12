f640ffb23117115eaa1767c7a5b5ab22d0a33bac
Anna <goryachevaanny@gmail.com>
2022-03-12 18:40:24 +0400

    Done

---
+++ Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Hello! What is your name?")
+    println("Hi! My name is Ann!")
+//Zdes comment
+    print("What your favorite food? ")
+    print(" Seafood. ")
+
+    print(" Where are you live? ")
+    println(" Georgia")
+}
